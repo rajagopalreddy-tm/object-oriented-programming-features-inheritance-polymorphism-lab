ab7b62ccc595a12a6bf43cc3d2c6ff70c91da338
rajagopalreddy-tm <rajagopoal.reddy@trainingmug.com>
2024-07-31 11:22:21 +0530

    test: remove solution

--- src/main/java/com/trainingmug/employee/app/Designer.java
+++ src/main/java/com/trainingmug/employee/app/Designer.java
@@ -1,45 +1,5 @@
 package com.trainingmug.employee.app;
 
-public class Designer extends Employee {
-
-    int noOfWebsites;
-
-    public Designer() {
-        super();
-    }
-
-    public Designer(long empId, String name, String designation, float grossSalary, float travellingAllowances,
-                    float federalTax, float stateTax, float incrementPercentage, int noOfWebsites) {
-        super(empId, name, designation, grossSalary, travellingAllowances, federalTax, stateTax, incrementPercentage);
-        this.noOfWebsites = noOfWebsites;
-    }
-
-    public void displayProfile() {
-        System.out.println("displayProfile() - Designer Class");
-        System.out.println("Id : " + empId);
-        System.out.println("Name : " + name);
-        System.out.println("Designation : " + designation);
-        System.out.println("Gross Salary : " + grossSalary);
-        System.out.println("Travelling Allowances : " + travellingAllowances);
-        System.out.println("Federal Tax : " + federalTax);
-        System.out.println("State Tax : " + stateTax);
-        System.out.println("Net Salary Increment Percentage : " + incrementPercentage);
-        System.out.println("Net Salary : " + this.calculateNetSalary());
-        System.out.println("Net Salary After Increment : " + this.calculateNetSalaryAfterIncrement());
-
-    }
-
-    float calculateNetSalary() {
-        System.out.println("calculateNetSalary() - Designer Class");
-        return grossSalary - federalTax - stateTax;
-    }
-
-    float calculateNetSalaryAfterIncrement() {
-        System.out.println("calculateNetSalaryAfterIncrement() - Designer Class");
-        float netSalary;
-        netSalary = calculateNetSalary();
-        netSalary += (netSalary * incrementPercentage / 100);
-        return netSalary;
-    }
+public class Designer {
 
 }
--- src/main/java/com/trainingmug/employee/app/Payroll.java
+++ src/main/java/com/trainingmug/employee/app/Payroll.java
@@ -2,29 +2,6 @@ package com.trainingmug.employee.app;
 
 public class Payroll {
 
-    void displayProfile(Employee employee) {
-        employee.displayProfile();
-    }
-
-    float calculateNetSalary(Employee employee) {
-        return employee.calculateNetSalary();
-    }
-
-    float calculateNetSalaryAfterIncrement(Employee employee) {
-        return employee.calculateNetSalaryAfterIncrement();
-    }
-
-    void displayProfile(int empId) {
-        System.out.println("This method display the employee profile with Employee ID");
-    }
-
-    void displayProfile(float fromSalaryRange,float toSalaryRange) {
-        System.out.println("This method display all employee profiles from and to given salary ranges");
-    }
-
-    void displayProfile(String department) {
-        System.out.println("This method display all the employee profiles from a given department");
-    }
 
 }
 
